73a5e3a9c663bdbcd6d9551669266003eacb4737
Arran France <arranf@users.noreply.github.com>
2015-10-29 14:53:53 +0000

    Change namespace and move out verify check as a separate method

--- IdealPostcodes.cs
+++ IdealPostcodes.cs
@@ -11,16 +11,18 @@ using Newtonsoft.Json;
 using RestSharp;
 
 using Rock.Attribute;
+using Rock.Address;
+using Rock;
 
-namespace Rock.Address
+namespace com.bricksandmortar.Address
 {
     /// <summary>
     /// The address lookup and geocoding service from <a href="https://ideal-postcodes.co.uk">Ideal Postcodes</a>
     /// </summary>
-    [Description( "An address verification and geocoding service from Ideal Postcodes" )]
-    [Export( typeof( VerificationComponent ) )]
-    [ExportMetadata( "ComponentName", "Ideal Postcodes" )]
-    [TextField( "API Key", "Your Ideal Postcodes API key (begins with ak_)", true, "", "", 2 )]
+    [Description("An address verification and geocoding service from Ideal Postcodes")]
+    [Export(typeof(VerificationComponent))]
+    [ExportMetadata("ComponentName", "Ideal Postcodes")]
+    [TextField("API Key", "Your Ideal Postcodes API key (begins with ak_)", true, "", "", 2)]
     public class IdealPostcodes : VerificationComponent
     {
         /// <summary>
@@ -33,47 +35,38 @@ namespace Rock.Address
         /// True/False value of whether the verification was successful or not
         /// </returns>
 
-        public override bool VerifyLocation( Rock.Model.Location location, bool reVerify, out string result )
+        public override bool VerifyLocation(Rock.Model.Location location, bool reVerify, out string result)
         {
             bool verified = false;
             result = string.Empty;
-            bool proceed = false;
 
-            VerifyCheck( location, reVerify, out proceed );
-
-            if ( proceed == true )
+            if (VerifyCheck(location, reVerify))
             {
 
-                string inputKey = GetAttributeValue( "APIKey" );
+                string inputKey = GetAttributeValue("APIKey");
                 string tags = null;
-                CreateTags( out tags );
+                CreateTags(out tags);
 
                 //Create address that encodes correctly
                 string inputAddress = null;
-                CreateInputAddress( location, out inputAddress );
+                CreateInputAddress(location, out inputAddress);
 
                 //restsharp API request
-                var client = new RestClient( "https://api.ideal-postcodes.co.uk/" );
-                var request = new RestRequest( Method.GET );
-                request.RequestFormat = DataFormat.Json;
-                request.Resource = "v1/addresses/";
-                request.AddParameter( "api_key", inputKey );
-                request.AddParameter( "query", inputAddress );
-                request.AddParameter( "limit", "1" );
-                request.AddParameter( "tags", tags );
-                var response = client.Execute( request );
+                var client = new RestClient("https://api.ideal-postcodes.co.uk/");
+                var request = BuildRequest(inputKey, inputAddress, tags);
+                var response = client.Execute(request);
 
-                if ( response.StatusCode == HttpStatusCode.OK )
+                if (response.StatusCode == HttpStatusCode.OK)
                 //Create a series of vars to make decoded response accessible
                 {
-                    var idealResponse = JsonConvert.DeserializeObject<RootObject>( response.Content );
+                    var idealResponse = JsonConvert.DeserializeObject<RootObject>(response.Content);
                     var idealAddress = idealResponse.result.hits;
-                    if ( idealAddress.Any() )
+                    if (idealAddress.Any())
                     {
                         var address = idealAddress.FirstOrDefault();
                         verified = true;
-                        result = string.Format( "Verified by Ideal Postcodes UDPRN: {0}", address.udprn );
-                        UpdateLocation( location, address );
+                        result = string.Format("Verified by Ideal Postcodes UDPRN: {0}", address.udprn);
+                        UpdateLocation(location, address);
                     }
                     else
                     {
@@ -97,62 +90,77 @@ namespace Rock.Address
             return verified;
         }
 
-        public void VerifyCheck( Rock.Model.Location location, bool reVerify, out bool verifyCheckResult )
+        private IRestRequest BuildRequest(string inputKey, string inputAddress, string tags)
         {
-            verifyCheckResult = false;
-            // Only verify if location is valid, has not been locked, and 
+            var request = new RestRequest(Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.Resource = "v1/addresses/";
+            request.AddParameter("api_key", inputKey);
+            request.AddParameter("query", inputAddress);
+            request.AddParameter("limit", "1");
+            request.AddParameter("tags", tags);
+            return request;
+        }
+
+        public bool VerifyCheck(Rock.Model.Location location, bool reVerify)
+        {
+            // Check if the location is valid, has not been locked, and 
             // has either never been attempted or last attempt was in last 30 secs (prev active service failed) or reverifying
-            if ( location != null &&
-                !( location.IsGeoPointLocked ?? false ) &&
+            if (location != null &&
+                !(location.IsGeoPointLocked ?? false) &&
                 (
                     !location.GeocodeAttemptedDateTime.HasValue ||
-                    location.GeocodeAttemptedDateTime.Value.CompareTo( RockDateTime.Now.AddSeconds( -30 ) ) > 0 ||
+                    location.GeocodeAttemptedDateTime.Value.CompareTo(RockDateTime.Now.AddSeconds(-30)) > 0 ||
                     reVerify
                 ) &&
                 location.Country == "GB" &&
-                ( string.IsNullOrWhiteSpace( location.Street1 ) || string.IsNullOrWhiteSpace( location.Street2 ) ) )
+                (string.IsNullOrWhiteSpace(location.Street1) || string.IsNullOrWhiteSpace(location.Street2)))
             {
-                verifyCheckResult = true;
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
-        public void CreateTags( out string tags )
+        public void CreateTags(out string tags)
         {
             tags = null;
-            var version = new Version( Rock.VersionInfo.VersionInfo.GetRockSemanticVersionNumber() );
-            System.Data.Odbc.OdbcConnectionStringBuilder builder = new System.Data.Odbc.OdbcConnectionStringBuilder( ConfigurationManager.ConnectionStrings["RockContext"].ConnectionString );
+            var version = new Version(Rock.VersionInfo.VersionInfo.GetRockSemanticVersionNumber());
+            System.Data.Odbc.OdbcConnectionStringBuilder builder = new System.Data.Odbc.OdbcConnectionStringBuilder(ConfigurationManager.ConnectionStrings["RockContext"].ConnectionString);
             object catalog = string.Empty;
-            if ( builder.TryGetValue( "initial catalog", out catalog ) )
+            if (builder.TryGetValue("initial catalog", out catalog))
             {
-                tags = string.Format( "{0},{1}", version, catalog );
+                tags = string.Format("{0},{1}", version, catalog);
             }
         }
 
-        public void CreateInputAddress( Rock.Model.Location location, out string inputAddress )
+        public void CreateInputAddress(Rock.Model.Location location, out string inputAddress)
         {
             var addressParts = new string[] { location.Street1, location.Street2, location.City, location.PostalCode };
-            inputAddress = string.Join( " ", addressParts.Where( s => !string.IsNullOrEmpty( s ) ) );
+            inputAddress = string.Join(" ", addressParts.Where(s => !string.IsNullOrEmpty(s)));
         }
 
-        public void UpdateLocation( Rock.Model.Location location, dynamic address )
+        public void UpdateLocation(Rock.Model.Location location, dynamic address)
         {
             location.Street1 = address.line_1;
             location.Street2 = address.line_2;
-            if ( !string.IsNullOrWhiteSpace( address.dependant_locality ) && address.dependant_locality != address.line_2 )
+            if (!string.IsNullOrWhiteSpace(address.dependant_locality) && address.dependant_locality != address.line_2)
             {
                 location.City = address.dependant_locality;
             }
             else
             {
                 string city = address.post_town;
-                city = CultureInfo.CurrentCulture.TextInfo.ToTitleCase( city.ToLower() );
+                city = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(city.ToLower());
                 location.City = city;
             }
 
             location.State = address.county;
             location.PostalCode = address.postcode;
             location.StandardizedDateTime = RockDateTime.Now;
-            location.SetLocationPointFromLatLong( address.latitude, address.longitude );
+            location.SetLocationPointFromLatLong(address.latitude, address.longitude);
             location.GeocodedDateTime = RockDateTime.Now;
         }
 
