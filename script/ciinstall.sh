29d1f064fc5e01d13de5028738c5c285ba22e274
Arran France <arranf@users.noreply.github.com>
2016-04-01 12:28:31 +0100

    Attempt to preserve attributes when copying

--- script/ciinstall.sh
+++ script/ciinstall.sh
@@ -4,8 +4,8 @@ set -e # halt script on error
 cd ..
 git clone --depth=3 --branch=gh-pages https://github.com/BricksandMortar/plugin-doc-template.git
 rm -rf plugin-doc-template/.git
-cp -r -n plugin-doc-template/* $TRAVIS_BUILD_DIR/
+cp -r -n -p plugin-doc-template/* $TRAVIS_BUILD_DIR/
 cd $TRAVIS_BUILD_DIR
 #gem install jekyll html-proofer rake rake-jekyll github-pages wdm
 bundle install
-sed -i -e 's/$VERSION_NUMBER/$TRAVIS_BUILD_NUMBER/g' ./_layouts/*.html
+sed -i -e 's/$VERSION_NUMBER/'$TRAVIS_BUILD_NUMBER'/g' ./_layouts/*.html
