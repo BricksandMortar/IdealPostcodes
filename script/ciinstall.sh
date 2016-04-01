5de30da7a005b87599afa25fde63222e4866e0a5
Arran France <arranf@gmail.com>
2016-04-01 07:48:45 +0100

    Add cleaning and checking out also removed Rakefile from the build

--- script/ciinstall.sh
+++ script/ciinstall.sh
@@ -2,7 +2,11 @@
 set -e # halt script on error
 
 cd ..
+rm -rf plugin-doc-template
 git clone --depth=3 --branch=gh-pages https://github.com/BricksandMortar/plugin-doc-template.git
+cd plugin-doc-template
+git checkout gh-pages
+cd ..
 rm -rf plugin-doc-template/.git
 cp -r -n plugin-doc-template/* $TRAVIS_BUILD_DIR/
 cd $TRAVIS_BUILD_DIR
