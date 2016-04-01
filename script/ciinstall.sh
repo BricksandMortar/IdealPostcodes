3b841ae603289fc754c5c4a50ff829f6ceb0efc5
Arran France <arranf@users.noreply.github.com>
2016-04-01 11:40:30 +0100

    Replace version number on CSS with Travis build number

--- script/ciinstall.sh
+++ script/ciinstall.sh
@@ -2,13 +2,10 @@
 set -e # halt script on error
 
 cd ..
-rm -rf plugin-doc-template
 git clone --depth=3 --branch=gh-pages https://github.com/BricksandMortar/plugin-doc-template.git
-cd plugin-doc-template
-git checkout gh-pages
-cd ..
 rm -rf plugin-doc-template/.git
 cp -r -n plugin-doc-template/* $TRAVIS_BUILD_DIR/
 cd $TRAVIS_BUILD_DIR
 #gem install jekyll html-proofer rake rake-jekyll github-pages wdm
 bundle install
+sed -i -e 's/$VERSION_NUMBER/$TRAVIS_BUILD_NUMBER/g' ./_layouts/*.html
